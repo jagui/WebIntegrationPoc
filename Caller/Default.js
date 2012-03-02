dcb03049022c0f9297f2557d1625e2dbc58c71d8
Juan Agui <juan@juanagui.com>
2012-03-02 23:01:09 +0100

    initial version: plain htmlh + jquery calling json enabled WCF service

---
+++ Caller/Default.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    // Add the page method call as an onclick handler for the div.
+    $("#Scan").click(function () {
+        jQuery.support.cors = true;
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:2995/Service.svc/Scan",
+            data: "{}",
+            dataType: "json",
+            error: function (xhr, status) {
+                $("#Result").val(xhr.statusText);
+            },
+            success: function (msg) {
+                // Replace the div's content with the page method's return.
+                $("#Result").val(msg.d);
+            }
+        });
+    });
+});
---
+++ WcfService/Service.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [WebGet]
+        [OperationContract]
+        public String Scan()
+        {
+            // Add your operation implementation here
+            return "hi";
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
